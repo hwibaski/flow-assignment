77fa3120cc848b90579f1a3723759173bacfed8e
hwibaski <hwibaski@gmail.com>
2022-01-03 02:13:50 +0900

    Add: README.md

--- client/src/Upload.js
+++ client/src/Upload.js
@@ -1,9 +1,13 @@
 function Upload() {
   return (
-    <div className="Upload">
-      <form action="/upload" method="post" enctype="multipart/form-data">
-        <input type="file" name="userfile" />
-        <button type="submit">업로드</button>
+    <div className='Upload'>
+      <form
+        action='http://localhost:8000/upload'
+        method='post'
+        enctype='multipart/form-data'
+      >
+        <input type='file' name='userfile' />
+        <button type='submit'>업로드</button>
       </form>
     </div>
   );
